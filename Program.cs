8deafa2b8dc59e29f3032457dc476df29d135ad1
aubint <aubint10@gmail.com>
2014-11-07 12:06:29 -0500

    AppPromedioNotas
    ejercicio promedio notas

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPromedioNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float nota1, nota2, nota3,promedio;
+            Console.WriteLine("PROMEDIO DE NOTAS!!");
+
+            Console.WriteLine("Ingrese nota 1: ");
+            nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese nota 2: ");
+            nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese nota 3: ");
+            nota3 = float.Parse(Console.ReadLine());
+
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+            if (promedio >= 3.0)
+            {
+                Console.WriteLine("PROMOCIONADO!!");
+            }
+            else
+            {
+                Console.WriteLine("PERDIO!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
